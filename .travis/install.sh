dc90b25f65246c3b25a0e17da3ba4c0f0986892c
Alejandro Barrera <alejandro.barrera@duke.edu>
2016-07-26 14:43:00 -0400

    remove voila references

--- .travis/install.sh
+++ .travis/install.sh
@@ -15,9 +15,3 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 else
   pip install 'setuptools>=18.5';
 fi
-
- voila deltapsi
-    --output /media/tyaoi4/10CA2BA3CA2B8456/MAJIQ_testRUN/MAJIQ_results/MacroGen/Delta_PSI_Analysis/Delta_PSI_VOILA
-    -splice-graphs1 /media/tyaoi4/10CA2BA3CA2B8456/MAJIQ_testRUN/MAJIQ_results/MacroGen/CT_27_accepted_hits.splicegraph
-    -splice-graphs2 /media/tyaoi4/10CA2BA3CA2B8456/MAJIQ_testRUN/MAJIQ_results/MacroGen/BPA_39_accepted_hits.splicegraph
-    /media/tyaoi4/10CA2BA3CA2B8456/MAJIQ_testRUN/MAJIQ_results/MacroGen/Delta_PSI_Analysis/CT_27_BPA_39.deltapsi_quantify.pickle
